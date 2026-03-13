25a762621a3e5e7ac685daaaeaaa1c9e2fea650d
EK257 <klime777@ya.ru>
2026-03-13 12:10:47 +0300

    Update Form1.Designer.cs

--- l13/Form1.Designer.cs
+++ l13/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace l13
+namespace l13
 {
     partial class Form1
     {
@@ -29,62 +29,33 @@
         private void InitializeComponent()
         {
             button1 = new Button();
-            button2 = new Button();
-            label1 = new Label();
             label2 = new Label();
-            textBox1 = new TextBox();
             label3 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(12, 56);
+            button1.Location = new Point(12, 12);
             button1.Name = "button1";
-            button1.Size = new Size(137, 23);
+            button1.Size = new Size(142, 23);
             button1.TabIndex = 0;
-            button1.Text = "Добавить посетителя";
+            button1.Text = "Добавить посетителей";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(12, 123);
-            button2.Name = "button2";
-            button2.Size = new Size(137, 23);
-            button2.TabIndex = 1;
-            button2.Text = "Кол-во посетителей";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
-            label1.Name = "label1";
-            label1.Size = new Size(31, 15);
-            label1.TabIndex = 2;
-            label1.Text = "Имя";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 92);
+            label2.Location = new Point(12, 51);
             label2.Name = "label2";
             label2.Size = new Size(78, 15);
             label2.TabIndex = 3;
             label2.Text = "Посетители: ";
             // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(12, 27);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(137, 23);
-            textBox1.TabIndex = 4;
-            // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 159);
+            label3.Location = new Point(12, 76);
             label3.Name = "label3";
             label3.Size = new Size(50, 15);
             label3.TabIndex = 5;
@@ -94,12 +65,9 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(334, 201);
+            ClientSize = new Size(235, 116);
             Controls.Add(label3);
-            Controls.Add(textBox1);
             Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
@@ -110,10 +78,7 @@
         #endregion
 
         private Button button1;
-        private Button button2;
-        private Label label1;
         private Label label2;
-        private TextBox textBox1;
         private Label label3;
     }
 }
